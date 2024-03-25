daecda4c442e537fed98059cb2ca48726ddaea39
Andrey <andreydias@alunos.utfpr.edu.br>
2024-03-25 13:25:21 -0300

    week 2 finished

---
+++ neural-networks-advanced-algorithms-course/week_2/public_tests.py
@@ -0,0 +1,59 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.activations import linear, sigmoid, relu
+
+
+def test_my_softmax(target):
+    z = np.array([1.0, 2.0, 3.0, 4.0])
+    a = target(z)
+    atf = tf.nn.softmax(z)
+
+    assert np.allclose(a, atf, atol=1e-10), f"Wrong values. Expected {atf}, got {a}"
+
+    z = np.array([np.log(0.1)] * 10)
+    a = target(z)
+    atf = tf.nn.softmax(z)
+
+    assert np.allclose(a, atf, atol=1e-10), f"Wrong values. Expected {atf}, got {a}"
+
+    print("\033[92m All tests passed.")
+
+
+def test_model(target, classes, input_size):
+    target.build(input_shape=(None, input_size))
+
+    # Print model input shape for debugging
+    print("Model input shape:", target.input.shape.as_list())
+
+    assert (
+        len(target.layers) == 3
+    ), f"Wrong number of layers. Expected 3 but got {len(target.layers)}"
+    assert target.input.shape.as_list() == [
+        None,
+        input_size,
+    ], f"Wrong input shape. Expected [None,  {input_size}] but got {target.input.shape.as_list()}"
+    i = 0
+    expected = [
+        [Dense, [None, 25], relu],
+        [Dense, [None, 15], relu],
+        [Dense, [None, classes], linear],
+    ]
+
+    for layer in target.layers:
+        # Print layer output shape for debugging
+        print(f"Layer {i + 1} output shape:", layer.output.shape.as_list())
+
+        assert (
+            type(layer) == expected[i][0]
+        ), f"Wrong type in layer {i}. Expected {expected[i][0]} but got {type(layer)}"
+        assert (
+            layer.output.shape.as_list() == expected[i][1]
+        ), f"Wrong number of units in layer {i}. Expected {expected[i][1]} but got {layer.output.shape.as_list()}"
+        assert (
+            layer.activation == expected[i][2]
+        ), f"Wrong activation in layer {i}. Expected {expected[i][2]} but got {layer.activation}"
+        i += 1
+
+    print("\033[92mAll tests passed!")
